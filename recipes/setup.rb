694db5126b21337ba0ea13efd2563369e5144dac
vagrant <vagrant@localhost.localdomain>
2018-02-04 18:39:32 +0000

    created a template to pull and insert motd from /etc/motd file"

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -8,8 +8,8 @@ package 'ntp' do
   action :install
 end
 
-file '/etc/motd' do
-  content "This system is the property of Author"
-  owner "root"
-  group "root"
+template '/etc/motd' do
+  action  :create
+  content "motd"
+ 
 end
